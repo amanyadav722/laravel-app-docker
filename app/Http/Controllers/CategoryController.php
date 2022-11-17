bc04782b137c39203b69e266a3b65e77738c029b
imjlabshetj <jordan.raphael@habitatetjardin.fr>
2022-11-17 10:57:02 +0100

    "Modification Category Controller"

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use Illuminate\Http\Request;
-use App\Http\Requests\StoreCategoryRequest;
-use App\Http\Requests\UpdateCategoryRequest;
 
 class CategoryController extends Controller
 {
