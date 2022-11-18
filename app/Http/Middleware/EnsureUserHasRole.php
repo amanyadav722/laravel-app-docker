fa33f24a928f4ff1dcdaffc37767705a96cbd2f4
imjlabshetj <jordan.raphael@habitatetjardin.fr>
2022-11-18 14:37:05 +0100

    Sécurité des routes Ok

--- app/Http/Middleware/EnsureUserHasRole.php
+++ app/Http/Middleware/EnsureUserHasRole.php
@@ -16,7 +16,7 @@ class EnsureUserHasRole
      */
     public function handle(Request $request, Closure $next, String $role)
     {
-        if($request->user()->role === $role) return $next($request);
+        if($request->user()->roles()->where('name', $role)->exists()) return $next($request);
 
         abort(403);
     }
