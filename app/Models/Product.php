f58f28da7436c5c01e79f09bc8b5753a5aecda74
imjlabshetj <jordan.raphael@habitatetjardin.fr>
2022-11-18 10:13:17 +0100

    "Relation HasMany Belongs To"

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
@@ -11,5 +12,10 @@ class Product extends Model
 
     protected $table = "products";
     protected $fillable = ["name","ean", "image_1", "image_2", "image_3", "image_4", "image_5", "created_at", "updated_at"];
+
+    public function category() 
+    {
+        return $this->belongsTo(Category::class);    
+    }
     
 }
