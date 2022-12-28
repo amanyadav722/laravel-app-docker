a4a2f2fd28f8bbebbc01d91a174c6e51e4096217
imjlabshetj <jordan.raphael@habitatetjardin.fr>
2022-12-28 14:28:00 +0100

    "Update Product form with new fields"

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,118 +2,69 @@
 
 namespace App\Http\Controllers;
 
-use SplTempFileObject;
+use App\Http\Controllers\Controller;
 use App\Models\Product;
 use App\Models\Category;
 use Illuminate\Http\Request;
-use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
-use NunoMaduro\Collision\Contracts\Writer;
 
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $products = Product::all();
-
         return view('products.index', compact('products'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function show(Product $product)
     {
-
-        $categories = Category::all();
-
-        return view('products.create')->with('categories', $categories);;
+        return view('products.show', compact('product'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function edit(Product $product)
+    {
+        $categories = Category::all();
+        return view('products.edit', compact('product', 'categories'));
+    }
+
+    public function update(Request $request, Product $product)
     {
         $request->validate([
-            'name' => 'required',
-            'description' => 'required',
-            'price' => 'required|numeric',
+            'name' => 'required|max:255',
+            'ean' => 'required|unique:products,ean,'.$product->id.'|max:13',
+            'short_description' => 'required|max:255',
+            'long_description' => 'required',
+            'image' => 'nullable|image|max:1500',
             'category_id' => 'required|exists:categories,id',
-            'image' => 'image|mimes:jpeg,png,jpg|max:1500|dimensions:max_width=1500,max_height=1500',
+            'status' => 'required|in:pending,validated,rejected'
         ]);
 
-        $product = new Product();
-        $product->name = $request->input('name');
-        $product->description = $request->input('description');
-        $product->price = $request->input('price');
-        $product->category_id = $request->input('category_id');
-        $product->user_id = auth()->user()->id;
-        $product->image = $request->image;
+        $product->name = $request->name;
+        $product->ean = $request->ean;
+        $product->short_description = $request->short_description;
+        $product->long_description = $request->long_description;
+        $product->category_id = $request->category_id;
+        $product->status = $request->status;
+
+        if ($request->hasFile('image')) {
+            $currentImage = $product->image;
+            if ($currentImage) {
+                Storage::delete('public/'.$currentImage);
+            }
+            $image = $request->file('image');
+            $path = $image->store('products', 'public');
+            $product->image = $path;
+        }
+
         $product->save();
 
-        return redirect('/products')->with('success', 'Produit enregistré avec succès');
+        return redirect()->route('products.index');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Product  $produit
-     * @return \Illuminate\Http\Response
-     */
-
-    public function show($id)
+    public function destroy(Product $product)
     {
-        $products = Product::find($id);
-        return view('products.show')->with('products', $products);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Product  $produit
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        $products = Product::find($id);
-        return view('products.edit')->with('products', $products);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Models\Product  $produit
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        $products = Product::find($id);
-        $input = $request->all();
-        $products->update($input);
-        return redirect('products');
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Product  $produit
-     * @return \Illuminate\Http\Response
-     */
-
-
-    public function destroy($id)
-    {
-        Product::destroy($id);
-        return redirect('products');
+        $product->delete();
+        return redirect()->route('products.index');
     }
 }
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -13,11 +13,6 @@
         <div class="card-body">
         <h5 class="card-title">Name : {{ $products->name }}</h5>
         <p class="card-text">EAN : {{ $products->ean }}</p>
-        <p class="card-text">Image 1 : {{ $products->image_1 }}</p>
-        <p class="card-text">Image 2 : {{ $products->image_2 }}</p>
-        <p class="card-text">Image 3 : {{ $products->image_3 }}</p>
-        <p class="card-text">Image 4 : {{ $products->image_4 }}</p>
-        <p class="card-text">Image 5 : {{ $products->image_5 }}</p>
   </div>
       
     </hr>
