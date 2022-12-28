7a8a19611754fcc81cc9b63a23eb2bebe3cb9a6c
imjlabshetj <jordan.raphael@habitatetjardin.fr>
2022-12-28 10:50:13 +0100

    "Error adding category_id to product"

--- app/Models/User.php
+++ app/Models/User.php
@@ -49,4 +49,9 @@ class User extends Authenticatable
         return $this->belongsToMany(Role::class);
     }
 
+    public function product() {
+
+        return $this->hasMany(Product::class);
+    }
+
 }
--- database/migrations/2022_11_14_152153_create_products_table.php
+++ database/migrations/2022_11_14_152153_create_products_table.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -22,9 +21,9 @@ return new class extends Migration
             $table->text('short_description')->nullable();
             $table->text('long_description')->nullable();
             $table->string('image_url')->nullable();
-            $table->unsignedBigInteger('category_id');
-            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
-            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('category_id')->index();
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->unsignedBigInteger('user_id')->index();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->string('status')->default('new');
             $table->timestamps();
